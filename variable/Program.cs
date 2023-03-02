73afd05a5ce2c8c37e8ee6a89e842847e1fd1dfd
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-02 16:18:38 -0500

    finished exercise

---
+++ variable/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// dataType variableName;
+int y;
+// variableName = value;
+y = -30;
+string dogName = "Ralph";
+int dogAge = -30;
+Console.WriteLine($"My dog's name is{dogName},He is{dogAge} years old");
